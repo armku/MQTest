69ad11d6440be6cc373fc8f0fef673b358df2175
Ace <258613763@qq.com>
2018-12-16 20:35:05 +0800

    版本号规范为0.1

--- Src/MQTest/Properties/AssemblyInfo.cs
+++ Src/MQTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("mqcl")]
+[assembly: AssemblyTitle("MQTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mqcl")]
+[assembly: AssemblyProduct("MQTest")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2018.1216")]
-[assembly: AssemblyFileVersion("1.0.2018.1216")]
+[assembly: AssemblyVersion("0.1.2018.1216")]
+[assembly: AssemblyFileVersion("0.1.2018.1216")]
